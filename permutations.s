76115e3bf2b640b4d35246d5239d93f5f7b8c82f
Malcolm Weaver <malcolm.weaver12128@gmail.com>
2020-09-10 14:10:54 -0700

    Initialize values

--- permutations.s
+++ permutations.s
@@ -8,32 +8,13 @@ format:
 
     .text
     .global _start
-_start:
-	# your code goes here
-    mov     $array, %rdi
-    // call    print_reg
-    // jmp     exit
-
-    // add     $16, %rdi
-    // mov     (%rdi), %rdi
-    // call    print_reg
-    //
-    // jmp     exit
-
-
-    mov     arr_len, %rsi
-    call    find_max
-
-    mov     %rax, %rdi
-    call    print_reg
-	// ret
-// asd main:
-//     push    %rbp
-//     push    %rbx
-//     push    %r12
-//
-//     mov     $0, %r12
-//     mov     arr_len, %rbx
+_start::
+    push    %rbp
+    push    %rbx
+    push    %r12
+p1:
+    mov     $0, %r12
+    mov     arr_len, %rbx
 // p2:
 //     mov     $array, %rdi
 //     mov     $arr_len, %rsi
