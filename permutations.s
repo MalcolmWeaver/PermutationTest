5c64b8317a2a19d8b2a076f19fe5799c741768be
Malcolm Weaver <malcolm.weaver12128@gmail.com>
2020-09-10 14:20:05 -0700

    P2

--- permutations.s
+++ permutations.s
@@ -8,23 +8,23 @@ format:
 
     .text
     .global _start
-_start::
+_start:
     push    %rbp
     push    %rbx
     push    %r12
 p1:
     mov     $0, %r12
     mov     arr_len, %rbx
-// p2:
-//     mov     $array, %rdi
-//     mov     $arr_len, %rsi
-//
-//     jmp    find_max
-// continue_p2:
-//
-//     mov     %rax, %rsi
-//     mov     $format, %rdi
-//     call    printf
+p2:
+    mov     $array, %rdi
+    mov     arr_len, %rsi
+    call    find_max
+
+    imul    %rbx, %r12
+    add     %rax, %r12
+
+    mov     %r12, %rdi
+    call    print_reg
 //
 //     pop     %r12
 //     pop     %rbx
